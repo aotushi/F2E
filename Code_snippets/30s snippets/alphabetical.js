aa7b6b35f9fb32743f1e55a4ebcfc3a58aee6728
sh <aotushi@hotmail.com>
2023-02-27 18:05:43 +0800

    docs

---
+++ Code_snippets/30s snippets/alphabetical.js
@@ -0,0 +1,20 @@
+/*
+title: Sort array alphabetically
+tags: array
+author: chalarangelo
+cover: boutique-home-office-1
+firstSeen: 2023-02-15T05:00:00-04:00
+*/
+
+// 基于给定的属性来排序字母表对象的数组
+
+// example
+const people = [{ name: "john" }, { name: "adam" }, { name: "mary" }];
+
+alphabetical(people, (g) => g.name); // [ { name: 'Adam' }, { name: 'John' }, { name: 'Mary' } ]
+
+
+
+// arr.sort((a,b) => a.name - b.name)
+
+
