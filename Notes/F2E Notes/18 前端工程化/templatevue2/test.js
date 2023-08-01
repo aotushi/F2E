ddce8776da2703add2fde51eeffc0e258bca24dd
sh <aotushi@hotmail.com>
2023-08-01 18:15:44 +0800

    docs

---
+++ Notes/F2E Notes/18 前端工程化/templatevue2/test.js
@@ -0,0 +1,7 @@
+let a = 2;
+let obj = {
+  a: "1",
+  b: "3"
+};
+
+console.log(a);
---
+++ Notes/F2E Notes/18 前端工程化/templatevue2/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "testComponent",
+  data() {
+    return {
+      msg: "test"
+    };
+  }
+};
+</script>
+
+<style scoped></style>
