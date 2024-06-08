7ef059d7d9c1626293a424eec7c4706e7da33bdf
sh <aotushi@hotmail.com>
2024-06-08 19:51:13 +0800

    p-react-m-3-5-useState同步异步

--- Program/React/react-manager/src/App.tsx
+++ Program/React/react-manager/src/App.tsx
@@ -2,46 +2,42 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { flushSync } from 'react-dom';
 
 function App() {
-    // const [count, setCount] = useState(0);
-    const style = { color: 'red', fontSize: '22px' };
-    let name = <span style={{ color: 'blue', fontSize: '50px' }}>Hello World!</span>;
-    let list = ['tom', 'dick', 'jack', 'lily'];
-    let inputValue = 'value22';
-    // let handleOnChange = (event: any) => {
-    //     console.log('event', event);
-    // };
     const [count, setCount] = useState(0);
     let handleOnClick = () => {
+        // 第一种方式 执行了两次console.log, 但count只增加了1
         // setCount(count + 1);
         // setCount(count + 1);
-        // setCount(count + 1);
-        setCount((count) => count+1);
-        setCount((count) => count+1);
-    };
-    console.log('render')
 
-    // const handleOnClickCount = () => {
-    //     setTimeout(() => {
-    //         setCount1(count + 1);
-    //     });
-    // };
+        // 第二种方式 执行了两次console.log, 但count增加了2
+        // setCount(() => count + 1);
+        // setCount(() => count + 1);
+
+        //第三种方式, 执行了两次console.log, count增加了1
+        // setTimeout(() => {
+        //     setCount(count + 1);
+        //     setCount(count + 1);
+        // });
+
+        // 同步更新使用flushSync
+        flushSync(() => {
+            setCount(() => count + 1);
+            setCount(() => count + 1);
+        });
+
+        // 同步更新使用flushSync 2
+        flushSync(() => {
+            setCount(() => count + 1);
+            setCount(() => count + 1);
+        });
+    };
+    console.log('render');
     return (
         <div>
-            {name}
-            <p>用户列表</p>
-            {/* <p>
-                {list.map(item => (
-                    <div key={item}>{item}</div>
-                ))}
-            </p> */}
-            <p>
-                {/* <input value={inputValue} onChange={handleOnChange}></input> */}
-            </p>
+            <p>数值是: {count}</p>
             <button onClick={handleOnClick}>函数调用{count}</button>
-            {/* <p>{count1}</p> */}
-            {/* <button onClick={handleOnClickCount}>增加count</button> */}
         </div>
     );
 }
