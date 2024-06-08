fef9d70bf71eac8210942f3a97744c64b3e64fff
sh <aotushi@hotmail.com>
2024-06-08 18:17:32 +0800

    p-react-3-4 code

--- Program/React/react-manager/src/App.tsx
+++ Program/React/react-manager/src/App.tsx
@@ -4,46 +4,58 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-    // const [count, setCount] = useState(0);
-    const style = { color: 'red', fontSize: '22px' };
-    let name = <span style={{ color: 'blue', fontSize: '50px' }}>Hello World!</span>;
-    let list = ['tom', 'dick', 'jack', 'lily'];
-    let inputValue = 'value22';
-    // let handleOnChange = (event: any) => {
-    //     console.log('event', event);
-    // };
-    const [count, setCount] = useState(0);
-    let handleOnClick = () => {
-        // setCount(count + 1);
-        // setCount(count + 1);
-        // setCount(count + 1);
-        setCount((count) => count+1);
-        setCount((count) => count+1);
-    };
-    console.log('render')
+  const [count, setCount] = useState(0);
+  const [user, setUser] = useState({ name: '河畔一角', age: 30 });
+  const [list, setList] = useState(['tom', 'jack', 'lucy']);
 
-    // const handleOnClickCount = () => {
-    //     setTimeout(() => {
-    //         setCount1(count + 1);
-    //     });
-    // };
-    return (
-        <div>
-            {name}
-            <p>用户列表</p>
-            {/* <p>
-                {list.map(item => (
-                    <div key={item}>{item}</div>
-                ))}
-            </p> */}
-            <p>
-                {/* <input value={inputValue} onChange={handleOnChange}></input> */}
-            </p>
-            <button onClick={handleOnClick}>函数调用{count}</button>
-            {/* <p>{count1}</p> */}
-            {/* <button onClick={handleOnClickCount}>增加count</button> */}
-        </div>
-    );
+  const updateUser = () => setUser({ ...user, age: 50 });
+  const handleList = () => {
+    // setList([...list, 'rose']);
+    // list.push('rose'); 不合法的写法
+  };
+  const handleCount = () => {
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+
+    // setTimeout(() => {
+    //   setCount(count + 1);
+    //   setCount(count + 1);
+    //   setCount(count + 1);
+    //   setCount(count + 1);
+    // });
+    setCount(() => {
+      count + 1;
+      count + 1;
+      count + 1;
+      count + 1;
+      return count + 1;
+    });
+  };
+
+  return (
+    <div className='App'>
+      <p>欢迎学习react后台管理</p>
+      <p>count: {count}</p>
+      <p>
+        <span>用户名称: {user.name}</span> <span>用户年龄: {user.age}</span>
+      </p>
+      <p>
+        {list.map(item => {
+          return (
+            <span key={item} style={{ marginRight: 10 }}>
+              {item}
+            </span>
+          );
+        })}
+      </p>
+      <p>
+        <button onClick={handleCount}>点击按钮修改值</button>
+        <button onClick={updateUser}>修改名称</button>
+        <button onClick={handleList}>新增用户</button>
+      </p>
+    </div>
+  );
 }
 
 export default App;
